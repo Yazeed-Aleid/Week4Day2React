5ccdc4094957eeb66b60d7212dca6df099c5e252
yazeed aleid <yazzed.aleid@gmail.com>
2021-10-25 17:15:31 +0300

    W4D2HW

---
+++ my-app/src/componants/Tweets.js
@@ -0,0 +1,19 @@
+function Tweets (){
+   let users =
+   [
+       {name:'Yazeed',pic:'https://www.pinclipart.com/picdir/middle/165-1653686_female-user-icon-png-download-user-colorful-icon.png',tweets:['السلام عليكم','كيف الحال']},
+       {name:'Khalid',pic:'https://toppng.com/uploads/preview/circled-user-female-skin-type-4-icon-pro-icon-115534084504dcnr2bmdl.png',tweets:[' Good Morning','hi ']},
+       {name:'Maha',pic:'https://cdn.imgbin.com/21/23/1/imgbin-computer-icons-female-user-profile-avatar-material-x1Zz1EDVQQssccaQu0dy0VFGy.jpg',tweets:['Whats up ','JS is good']}
+   ]
+   return(
+       <div >
+           {users.map(x =><div>
+            <h1>{x.name}</h1>
+            <img src = {x.pic}/>
+             {x.tweets.map(y => <li>{y}</li>)  }
+           </div>  )}
+           
+       </div>
+   )
+}
+export default Tweets
